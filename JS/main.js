731862b8c915dd121ca170ffbaa6ebd1d2229e70
NicolasZaffaroni <nicolasmzaffaroni@gmail.com>
2023-05-09 23:24:11 +0200

    luchando contra la funcion

--- JS/main.js
+++ JS/main.js
@@ -34,42 +34,14 @@ class Paciente{
     this.modalidad = informacion_paciente.modalidad ;
     this.dia = informacion_paciente.dia;
     this.horario = informacion_paciente.horario ;
-    this.turnoconfirmado= false ;
-    
 }
-calendario (){
-    if ( dia ==="sabado"|| dia === "domingo"){ 
-        return alert("Los fines de semana Espacio Fibra permanece cerrado");}       
-    else if ( horario <15 || horario >20 ){
-        return alert("El horario disponible para seleccionar una cita es de 15 a 20hs");}
-    else if ( modalidad === "online" && modalidad === "presencial"){
-        return modalidad => 
-            {let index1 = pacientes_online.push(modalidad);
-            let  index2 = pacientes_presencial.push(modalidad);
-            if (index1==="online" ){
-                return alert("A seleccionado la modalidad online");
-            }
-            
-            else if (index2==="presencial"){
-                return alert("A seleccionado la modalidad presencial");
-            }
-        
-            else {
-                return alert("Disculpas, solo ofrecemos dos modalidades: online o presencial.");
-            }
-        }
-    }
-    else if(confirmar === "no" ){
-        return alert("Entendido. Si desea solicitar una cita personalizada, por favor contáctenos a través del formulario de contacto. ¡Gracias por considerarnos!");}
-    else{   this.turnoconfirmado=true;
-            alert( ` Gracias, por confiar en Espacio Fibra, ${nombre}, el dia seleccionado es ${dia}, en el horario ${horario} y la modalidad sera ${modalidad},`)
-            ;}
-};}
+;}
 const nombre = prompt("Indique su nombre ?");
 
 const apellido = prompt ("Cual es su apellido ?");
 
-const dia = prompt(" Espacio Fibra a tiende de Lunes a Viernes indique que día será más conveniente para usted");
+let dia = prompt(" Espacio Fibra a tiende de Lunes a Viernes indique que día será más conveniente para usted");
+const dias  = ["Lunes",'Martes','Miercoles','Jueves','Viernes'];
 
 const horario =  parseInt(prompt(" Espacio fibra atiende de 15hs a 20hs indique que franja horaria en términos de una hora le convendría, ej-- 19 "));
 const modalidad = prompt("Que tipo de sesión le convendría espaxio fibra pose dos modalidades Online y presencial");
@@ -78,7 +50,26 @@ const modalidad = prompt("Que tipo de sesión le convendría espaxio fibra pose 
 const confirmar = prompt (`  Desea confirmar la cita para el ${dia} en horario ${horario} y modalidad ${modalidad}, a nombre de ${nombre} ${apellido} ? `);
 
 
-const paciente1 = new Paciente(nombre, apellido, dia,horario,modalidad, confirmar);
+
+
+function calendario(){
+    while( dias.indexOf(dia)=== -1){
+        return dia = prompt(" Los dias sabado y domingo Espacio Fibra permanecera, cerrado, por favor vuelva a ingresar un dia de lunes a viernes");}       
+    while( horario <15 || horario >20 ){
+        alert("El horario disponible para seleccionar una cita es de 15 a 20hs");}
+    if (modalidad !== "online"  ||  modalidad !== "presencial") {
+            return alert("Lo sentimos, solo ofrecemos dos modalidades: online o presencial.");}
+    else if(confirmar === "no" ){
+        return alert("Entendido. Si desea solicitar una cita personalizada, por favor contáctenos a través del formulario de contacto. ¡Gracias por considerarnos!");}
+    else{ 
+            alert( ` Gracias, por confiar en Espacio Fibra, ${nombre} ${apellido}, el dia seleccionado es ${dia}, en el horario ${horario} y la modalidad sera ${modalidad},`)
+            ;}
+
+}
+
+
+
+const paciente1 = new Paciente(nombre, apellido, dia,horario,modalidad);
 
 
 
@@ -86,12 +77,7 @@ const paciente1 = new Paciente(nombre, apellido, dia,horario,modalidad, confirma
 
 
 
-paciente1.calendario();
-alert(`${nombre},${apellido}${dia}${horario}${modalidad}`)
-
-
-const pacientes_presencial = [];
-const pacientes_online = []
+calendario();
 
 
 
@@ -99,8 +85,70 @@ const pacientes_online = []
 
 
 
+/*const pacientes =[
+
+    {nombre: "jose", apellido:"zaragoza", dia:"lunes", horario:"15hs", modalidad:"online"},
+    {nombre: "carlos", apellido:"catamarca", dia:"martes", horario:"14hs", modalidad:"presencial"},
+    {nombre: "sofia", apellido:"gala", dia:"miercoles", horario:"16hs", modalidad:"presencial"},
+    {nombre: "arturo", apellido:"duro", dia:"jueves", horario:"19hs", modalidad:"online"},
+    {nombre: "esteban", apellido:"quito", dia:"lunes", horario:"20hs", modalidad:"presencial"},
+    {nombre: "camila", apellido:"camilin", dia:"viernes", horario:"16hs", modalidad:"online"},
+    {nombre: "juana", apellido:"loca", dia:"martes", horario:"17hs", modalidad:"online"},
+    {nombre: "nicolas", apellido:"rodriguez", dia:"jueves", horario:"15hs", modalidad:"presencial"},
+    {nombre: "miguel", apellido:"cambaceres", dia:"miercoles", horario:"20hs", modalidad:"presencial"},
+    {nombre: "emma", apellido:"rioja", dia:"lunes", horario:"17hs", modalidad:"online"},
+];
 
 
+
+//forEach para recorrer los array 
+//pacientes.forEach(item =>{
+//    console.log(item) 
+//} );
+
+
+
+
+//find encuentra el primer elemento que cumpla la condición
+/*
+let modalidad= prompt("ingrese la modalidad de su turno");
+const encontrado = pacientes.find(turno => turno.modalidad === modalidad);
+
+if (encontrado) {
+let mensaje = `
+    Nombre: ${encontrado.nombre}
+    Apellido: ${encontrado.apellido}
+    Dia: ${encontrado.dia}
+    Modalidad: ${encontrado.modalidad}
+    Horario: ${encontrado.horario}
+`;
+
+alert(mensaje);
+} else {
+alert("Producto no encontrado");
+}
+
+
+*/
+
+//filter encuentra los elementos que cumplan la condicion dada
+/* EN ESTE EJEMPLO SOLO MUESTRA LOS TURNOS CON LAS MODALIDAD QUE ES INGRESADA POR EL USUARIO
+let modalidad= prompt("ingrese la modalidad de su turno");
+const encontrado = pacientes.filter((turno) => turno.modalidad.includes(modalidad) );
+
+encontrado.forEach ((turno) => {
+let mensaje = `
+    Nombre: ${turno.nombre}
+    Apellido: ${turno.apellido}
+    Dia: ${turno.dia}
+    Modalidad: ${turno.modalidad}
+    Horario: ${turno.horario}
+`;
+
+    alert(mensaje);
+} );
+
+*/
 
 
 
