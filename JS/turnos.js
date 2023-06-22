a926368bf83905f80b57aa75dd7dc17474141d26
NicolasZaffaroni <nicolasmzaffaroni@gmail.com>
2023-06-22 21:45:14 +0200

    FINALIZADO

--- JS/turnos.js
+++ JS/turnos.js
@@ -11,14 +11,14 @@ botonConsulta.addEventListener("click", () => {
     }
 
     let turnoEncontrado = formularios.some((turno) => turno.apellido === consultaTurno);
-
+//SOLO MUESTRA EL TURNO QUE CUMPLA CON LAS CONDICIONES
     if (turnoEncontrado) {
         let paciente = formularios.find(paciente =>paciente.apellido===consultaTurno);
         let contenedorMensaje = document.getElementById("contenedorMensaje");
     
 
 
-
+//MUESTRA EL TURNO
     let div = document.createElement("div");
     div.innerHTML = `
         <h2 class="titulo">Estimad@ ${paciente.nombre}</h2>
@@ -30,7 +30,7 @@ botonConsulta.addEventListener("click", () => {
     contenedorMensaje.append(div);
 
     let contenedorCancelar = document.getElementById("contenedorCancelar");
-
+//BOTON PARA CANCELAR TURNO
     let divCancelar = document.createElement("divCancelar");
     divCancelar.innerHTML = `
         <label for="cancelar">Cancele su turno !</label>
@@ -39,10 +39,47 @@ botonConsulta.addEventListener("click", () => {
     contenedorCancelar.append(divCancelar);
     let botonElimina = document.getElementById("botonElimina");
     setInterval("location.reload()", 5000);
-
+//SE ELIMINA EL TURNO DEL LOCALSTORAGE 
     botonElimina.addEventListener("click", () => {
-        // Código para eliminar el turno
-    });
+        consultaTurno = document.getElementById("apellido")?.value;
+            formularios = [];
+            turnoPactado = localStorage.getItem("formularios");
+            if(turnoPactado){
+                formularios =  JSON.parse(turnoPactado);
+                console.log("entramos2");}
+                turnoEncontrado =  formularios.some((turno)=>turno.apellido ===consultaTurno);
+                if (turnoEncontrado){
+                    turnoEncontrado =  formularios.findIndex((turno)=>turno.apellido ===consultaTurno);
+                if (turnoEncontrado !==-1){
+                    Swal.fire({
+                        title: 'Estas seguro que, quiere cancelar tu turno ?',
+                        text: "Si el turno, es menos de 24hs, se solicitara el abono del mismo!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Si, quiero cancelarlo!',}).then((result) =>{
+                            if (result.isConfirmed) {
+                            localStorage.removeItem("formulario");
+                            formularios.splice(turnoEncontrado, 1);
+                            console.log("ELIMINADO",formularios);
+                            localStorage.setItem("formularios", JSON.stringify(formularios))
+                            Swal.fire(
+                                'Cancelado!',
+                                'Esperamos poder volver a brindarle, otro turno, en el futuro!.',
+                                'success',)
+                                setInterval("location.reload()",3000)  }});// SE CIERRAN LOS IF PRA ELIMINAR TURNO DE LOCALSTORAGE
+                        }else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: 'No está cargado ese turno, enviamos un mensaje, en nuestra seccion de contacto y  a la brevedad lo solucionaremos!',
+                                footer: '<a href="./sesion.html">Solicita tu turno nuevamente!</a>'
+                            })
+                            setInterval("location.reload()",5000);}
+                            }
+                    });
+    //MUESTRA EL TURNO EN "SERVIDOR" CARGADO PREVIAMENTE MEDIANTE UNA PROMESA
     } else {
     fetch("../JS/pacientes.json")
         .then((response) => response.json())
